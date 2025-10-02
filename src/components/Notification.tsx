4f9e680ff08be4867ffdccf31938ed471b921645
Brenio Filho <brenio.filho@GERBIT.COM.BR>
2025-10-02 17:48:55 -0300

    add: added initial files to notifications

---
+++ src/components/Notification.tsx
@@ -0,0 +1,13 @@
+function Notification(){
+    
+
+    return (
+    <>
+        <div className="notificationDiv">
+            <h4></h4>
+        </div>    
+    </>
+    )
+}
+
+export default Notification
