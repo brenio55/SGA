15d45b76abae196ffa8ddb926d77fd1bb2302e6d
jul10l1r4 <jul10l1r4@disroot.org>
2025-12-13 21:48:11 -0300

    :pick: Use SSL only production envirovment

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
-const db = postgres(connectionString, { ssl: { rejectUnauthorized: false } });
+
+const isProduction = process.env.NODE_ENV === 'production';
+const db = postgres(connectionString, {
+    ssl: isProduction ? { rejectUnauthorized: false } : false
+});
 console.log('Database url:', connectionString);
 
 export default { db };
